0e3d722351233c013f9b14b754bfc789f5d32960
MD. Shohel Rana <shohel065931@gmail.com>
2025-11-06 08:08:04 +0600

    h

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php wp_head(); ?>
 </head>
 <body>
-    
+   
+
+
+
+<?php wp_footer(); ?>
 </body>
 </html>
